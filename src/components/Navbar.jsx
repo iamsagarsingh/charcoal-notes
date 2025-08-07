570665e0b69a80a2447bb3d80c3d49530b234227
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2025-08-07 19:01:24 +0530

    fixed Auth import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/UserContext";
-import { authService } from "../appwrite/auth";
+import { authService } from "../appwrite/Auth";
 
 const Navbar = () => {
     const {state, dispatch} = useAuth()
