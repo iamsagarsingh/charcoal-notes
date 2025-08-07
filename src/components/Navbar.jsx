6f5e7677663298497446ae9d31310cb2a81c6d76
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2025-08-07 19:55:28 +0530

    fix styling on small devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
         navigate('/login')
     }
   return (
-    <nav className="bg-blue-200 text-black shadow-[4px_4px_0px_rgba(0,0,0,1)] border-4 border-black px-2 py-2 md:px-4 md:py-4 flex items-center justify-between mx-2">
-      <div className="text-lg md:text-2xl font-extrabold tracking-wide">
-        <span className="bg-white border-4 border-black px-3 py-1 shadow-[3px_3px_0px_rgba(0,0,0,1)]">
+    <nav className="bg-blue-200 text-black shadow-[4px_4px_0px_rgba(0,0,0,1)] border-4 border-black px-1 py-2 md:px-4 md:py-4 flex items-center justify-between mx-2">
+      <div className="text-sm md:text-2xl font-extrabold tracking-wide flex-1">
+        <span className="bg-white border-4 border-black md:px-3 px-1 py-1 shadow-[3px_3px_0px_rgba(0,0,0,1)]">
           Charcoal-Notes
         </span>
       </div>
@@ -22,11 +22,11 @@ const Navbar = () => {
         {
             state.status ? 
             (
-                <ul className="flex gap-4 font-semibold items-center">
-                <li className="text-2xl font-bold">
+                <ul className="flex md:gap-4 gap-2 font-semibold items-center">
+                <li className="md:text-2xl text-sm font-bold">
                     {state?.userData?.name}
                 </li>
-          <li className="bg-white border-4 border-black px-4 py-1 shadow-[3px_3px_0px_rgba(0,0,0,1)] hover:bg-gray-100 cursor-pointer transition" onClick={handleLogout}>
+          <li className="bg-white border-4 border-black md:px-4 px-1 py-0.5 text-sm shadow-[3px_3px_0px_rgba(0,0,0,1)] hover:bg-gray-100 cursor-pointer transition" onClick={handleLogout}>
             Logout
           </li>
                 </ul>
